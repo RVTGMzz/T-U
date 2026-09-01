715a008d826238db004322de42ad18f0a53bb6f6
ronvotri <lengochung28191@gmail.com>
2026-09-01 14:32:44 +0700

    Point one-click build to alpha.3.1

--- BUILD_TEAM_UP.bat
+++ BUILD_TEAM_UP.bat
@@ -3,7 +3,7 @@ setlocal
 cd /d "%~dp0"
 
 echo ===============================================
-echo   Team Up! v0.1.0-alpha.3 - ONE CLICK BUILD
+echo   Team Up! v0.1.0-alpha.3.1 - ONE CLICK BUILD
 echo ===============================================
 echo.
 
