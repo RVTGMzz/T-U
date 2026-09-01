204a1f73fa87bb17552be2f3828f187881db5731
ronvotri <lengochung28191@gmail.com>
2026-09-01 10:50:09 +0700

    Persist linked companions separately

--- src/TeamUp/Core/PartySaveData.cs
+++ src/TeamUp/Core/PartySaveData.cs
@@ -2,5 +2,9 @@ namespace Ronvotri.TeamUp.Core;
 
 public sealed class PartySaveData
 {
+    public int SchemaVersion { get; set; } = 2;
+
     public List<PartyMemberData> Members { get; set; } = new();
+
+    public List<CompanionUnitData> CompanionUnits { get; set; } = new();
 }
