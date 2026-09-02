6f373cfb3b7e951501135cccc388e3c23a101dbd
ronvotri <lengochung28191@gmail.com>
2026-09-03 00:49:46 +0700

    Make Alpha 6.1 debug command registration visible

--- _build_support/IntegrateAlpha61DebugHarness.ps1
+++ _build_support/IntegrateAlpha61DebugHarness.ps1
@@ -38,9 +38,16 @@ if (-not $mod.Contains('DebugTools.RegisterCommands();')) {
             Alpha6Polish,
             SavePartyNow);
         DebugTools.RegisterCommands();
+        Monitor.Log("Team Up DEBUG HARNESS READY | command: teamup_test | build: v0.2.0-alpha.6.1.1", LogLevel.Info);
 '@
     $mod = Replace-Required $mod $old $new.TrimEnd() 'debug service construction'
 }
+elseif (-not $mod.Contains('Team Up DEBUG HARNESS READY')) {
+    $mod = Replace-Required $mod `
+        '        DebugTools.RegisterCommands();' `
+        "        DebugTools.RegisterCommands();`r`n        Monitor.Log(\"Team Up DEBUG HARNESS READY | command: teamup_test | build: v0.2.0-alpha.6.1.1\", LogLevel.Info);" `
+        'debug ready marker'
+}
 
 [System.IO.File]::WriteAllText($modPath, $mod, $utf8NoBom)
-Write-Host 'Alpha 6.1 Team Up debug harness integrated.'
+Write-Host 'Alpha 6.1.1 Team Up debug harness integrated with startup marker.'
