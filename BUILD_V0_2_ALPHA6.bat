1459108db435cbb4849c6d299401c00ed63bca55
ronvotri <lengochung28191@gmail.com>
2026-09-03 00:21:15 +0700

    Label one-click build as alpha 6.1 hotfix

--- BUILD_V0_2_ALPHA6.bat
+++ BUILD_V0_2_ALPHA6.bat
@@ -3,7 +3,7 @@ setlocal
 cd /d "%~dp0"
 
 echo =========================================================
-echo   Team Up! v0.2.0-alpha.6 - SIGNATURE + RESCUE + POLISH
+echo   Team Up! v0.2.0-alpha.6.1 - FOLLOW + CODEX + I18N HOTFIX
 echo =========================================================
 echo.
 
