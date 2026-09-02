cd47767c695fa18aa0053348c8a08c734ab0d597
ronvotri <lengochung28191@gmail.com>
2026-09-02 15:41:53 +0700

    Bump party save schema for progression and equipment

--- src/TeamUp/Core/PartySaveData.cs
+++ src/TeamUp/Core/PartySaveData.cs
@@ -2,7 +2,7 @@ namespace Ronvotri.TeamUp.Core;
 
 public sealed class PartySaveData
 {
-    public int SchemaVersion { get; set; } = 3;
+    public int SchemaVersion { get; set; } = 4;
 
     public List<PartyMemberData> Members { get; set; } = new();
 
