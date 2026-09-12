95a46282b153ace803c1b19c6f565ecd47f16fa5
ronvotri <lengochung28191@gmail.com>
2026-09-12 20:29:09 +0700

    diag: expose Pelipper mutation bridge status

--- src/TeamUp/ModEntry.Alpha6719.cs
+++ src/TeamUp/ModEntry.Alpha6719.cs
@@ -67,6 +67,8 @@ public sealed partial class ModEntry
         {
             case "status":
                 Monitor.Log(MutationAlpha6719.Describe(), LogLevel.Info);
+                if (PelipperRuntimeAlpha67446 is not null)
+                    Monitor.Log(PelipperRuntimeAlpha67446.DescribeMutationBridge(), LogLevel.Info);
                 Monitor.Log(MutationAlpha6719.LastMutationLine, LogLevel.Info);
                 return;
 
