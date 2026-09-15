cc55a94d503a9cf8cf90de7a2fcad55b79a62bf2
ronvotri <lengochung28191@gmail.com>
2026-09-15 20:36:05 +0700

    diag: include Pelipper Mutation hostility in status

--- src/TeamUp/ModEntry.Alpha6719.cs
+++ src/TeamUp/ModEntry.Alpha6719.cs
@@ -53,6 +53,7 @@ public sealed partial class ModEntry
         NativeMutationMinionsAlpha674418?.ResetTelemetry();
         PelipperSpawnCommandGateAlpha674419?.ResetTelemetry();
         MutationAggroAlpha674420?.ResetTelemetry();
+        PelipperMutationHostilityAlpha674421?.ResetTelemetry();
     }
 
     private void OnAlpha6719UpdateTicked(object? sender, UpdateTickedEventArgs e)
@@ -96,6 +97,8 @@ public sealed partial class ModEntry
                     Monitor.Log(PelipperSpawnCommandGateAlpha674419.Describe(), LogLevel.Info);
                 if (MutationAggroAlpha674420 is not null)
                     Monitor.Log(MutationAggroAlpha674420.Describe(), LogLevel.Info);
+                if (PelipperMutationHostilityAlpha674421 is not null)
+                    Monitor.Log(PelipperMutationHostilityAlpha674421.Describe(), LogLevel.Info);
                 if (MutationMinionSpawnAlpha674413 is not null)
                     Monitor.Log(MutationMinionSpawnAlpha674413.Describe(), LogLevel.Info);
                 if (PelipperSourceProbeAlpha67449 is not null)
